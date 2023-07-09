a83b45075e7ba6dc803e36b04117e86719332689
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-09 19:04:09 +0300

    metotlar, call by value vs reference

---
+++ c#-patika-dev/metotlar/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //erisim_belirteci geri_donustipi metot_adi(parametreListesi/arguman)
+              //{
+                //komutlar
+                //}
+            //erisim_belirteci: public, private, internal, protected
+
+            //geri_donustipi: void, int, string, double, bool...
+            
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+            
+
+
+        
+
+        }
+
+       static int Topla(int deger1, int deger2)
+        {
+            return deger1 + deger2;
+        }
+
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+
+}
