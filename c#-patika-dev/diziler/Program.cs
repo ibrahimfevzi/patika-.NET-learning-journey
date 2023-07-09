ab9f22f7438f1d4ae45f9afd553dddeb519c2468
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-09 18:09:47 +0300

    diziler

---
+++ c#-patika-dev/diziler/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string [] renkler = new string[5];
+
+            string[] hayvanlar = {"Kedi", "Köpek", "Kuş", "Maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            // Dizilere değer atama ve erişim
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            // Döngülerle dizi kullanımı
+            // Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz: ", i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+
+            Console.WriteLine("Ortalama: " + toplam/diziUzunlugu);
+            
+
+
+        }
+
+
+
+
+    }
+
+}
