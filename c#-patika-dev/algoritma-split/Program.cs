5e5ae7d51cbfc912183306671448f1d99027c84d
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-16 16:54:36 +0300

    algoritma split

---
+++ c#-patika-dev/algoritma-split/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StringKarakterCikartma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String ifadeyi ve indexi (örn: Algoritma,3): ");
+            string input = Console.ReadLine();
+
+            string[] inputParts = input.Split(',');
+            if (inputParts.Length == 2)
+            {
+                string ifade = inputParts[0].Trim();
+                int index = Convert.ToInt32(inputParts[1].Trim());
+
+                if (index >= 0 && index < ifade.Length)
+                {
+                    string sonuc = ifade.Remove(index, 1);
+                    Console.WriteLine("Sonuç: " + sonuc);
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz index değeri.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş formatı.");
+            }
+        }
+    }
+}
