c37bbd0faba685314d32dffb3c3dc9abe6acb1cf
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-16 16:57:55 +0300

    tersine çevir sorusu

---
+++ c#-patika-dev/tersini-yazdir/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KelimeleriTersCevirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String ifadeyi girin: ");
+            string input = Console.ReadLine();
+
+            string[] kelimeler = input.Split(' ');
+
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                char[] karakterler = kelimeler[i].ToCharArray();
+                Array.Reverse(karakterler);
+                kelimeler[i] = new string(karakterler);
+            }
+
+            string sonuc = string.Join(" ", kelimeler);
+            Console.WriteLine("Sonuç: " + sonuc);
+        }
+    }
+}
