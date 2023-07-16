cebe72d1a72d8dc79b8fbb90f4dff11f97eb2b9d
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-16 16:45:12 +0300

    ucgen çiz

---
+++ c#-patika-dev/ucgen-ciz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UcgenCizimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgenin boyutunu girin: ");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            UcgenCizici ucgenCizici = new UcgenCizici();
+            ucgenCizici.UcgenCiz(boyut);
+        }
+    }
+
+    class UcgenCizici
+    {
+        public void UcgenCiz(int boyut)
+        {
+            for (int satir = 1; satir <= boyut; satir++)
+            {
+                // Boşlukları yazdır
+                for (int bosluk = 1; bosluk <= boyut - satir; bosluk++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Yıldızları yazdır
+                for (int yildiz = 1; yildiz <= 2 * satir - 1; yildiz++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
