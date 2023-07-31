04ba9f97dffe5600eb3b1c315a046c59ee7788b2
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-31 19:53:00 +0300

    get ve getByID yazıldı

--- pratikler/hello/HelloWebapi/Controllers/WeatherForecastController.cs
+++ pratikler/hello/HelloWebapi/Controllers/WeatherForecastController.cs
@@ -1,78 +1,64 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace HelloWebapi.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+namespace HelloWebapi.Controllers
 {
-    private static readonly string[] Summaries = new[]
+    [ApiController]
+    [Route("[controller]s")]
+    public class WeatherForecastController : ControllerBase
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        private static readonly string[] Summaries = new[]
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-    [HttpGet]
-    //api/WeatherForecast?id=1
-    public WeatherForecast GetById([FromQuery] int id)
-    {
-         var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            _logger = logger;
+        }
 
-    if (id >= 1 && id <= weatherForecasts.Length)
-    {
-        return weatherForecasts[id - 1];
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public  WeatherForecast GetById(int id)
+        {
+            var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            if (id >= 1 && id <= weatherForecasts.Length)
+            {
+                return weatherForecasts[id - 1];
+            }
+
+            // If a weather forecast with the specified id is not found, return null or handle it as needed.
+            return null;
+        }
     }
 
-    // İlgili kimlik numarasına sahip hava durumu tahmini bulunamadıysa null dönebilirsiniz veya özel bir durum döndürebilirsiniz.
-    return null;
-}
-
-[HttpGet("{id}")]
-// /api/WeatherForecast/1
-public WeatherForecast GetForecast([FromRoute] int id)
-{
-    var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    public class WeatherForecast
     {
-        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        TemperatureC = Random.Shared.Next(-20, 55),
-        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    })
-    .ToArray();
-
-    if (id >= 1 && id <= weatherForecasts.Length)
-    {
-        return weatherForecasts[id - 1];
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
     }
-
-    // İlgili kimlik numarasına sahip hava durumu tahmini bulunamadıysa null dönebilirsiniz veya özel bir durum döndürebilirsiniz.
-    return null;
 }
-
-
-}
-
-
